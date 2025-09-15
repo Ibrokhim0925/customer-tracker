71ff92bc84a1529960c190328d8d05a34c4eac46
Ibrokhim0925 <ibrokhimushurov@usf.edu>
2025-09-14 23:33:58 -0400

    Add files via upload

--- cc_3a.js
+++ cc_3a.js
@@ -16,3 +16,26 @@ let Customers = [
         purchases: ["Tier", "Rear Window", "Security Camera"]
     }
 ];
+
+// Step 3: Add and Remove Data
+Customers.push({ name: "John Smith", 
+        email: "John.S@example.com",
+        purchases: ["Glass Bottle", "Jeans", "Sneakers"]
+}
+);
+
+let RemovedCustomer = Customers.shift();
+console.log(customers); 
+
+// Step 4: Update Customer Info
+Customers[1].email = "Smith.John@example.com";
+Customers[2].purchases.push("Window Wiper");
+
+console.log(customers);
+
+//  Step 5: Display Customer Information
+Customers.forEach((customer) => {
+    console.log(
+        `Name: ${Customer.name}, Email: ${customer.email}, Purchases: ${customer.purchases.length}`
+    );
+});
